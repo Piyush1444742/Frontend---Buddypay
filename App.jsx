841f54371c54a5b995e3ee1a2b16a2ee3d682552
Piyush Tupat <piyusheduc.id2004@gmail.com>
2026-01-14 19:46:26 +0530

    Updated

--- App.jsx
+++ App.jsx
@@ -4,6 +4,8 @@ import AuthPage from "./components/AuthPage";
 import SignupRole from "./pages/SignupRole";
 import ParentSignup from "./pages/ParentSignup";
 import ChildSignup from "./pages/ChildSignup";
+import ParentDashboard from "./pages/ParentDashboard";
+import ChildDashboard from "./pages/ChildDashboard";
 
 
 export default function App() {
@@ -13,8 +15,9 @@ export default function App() {
       <Route path="/auth" element={<AuthPage />} />
       <Route path="/signup-role" element={<SignupRole />} />
       <Route path="/signup/parent" element={<ParentSignup />} />
-<Route path="/signup/child" element={<ChildSignup />} />
-
+      <Route path="/signup/child" element={<ChildSignup />} />
+      <Route path="/parent-dashboard" element={<ParentDashboard />} />
+      <Route path="/child-dashboard" element={<ChildDashboard />} />
     </Routes>
   );
 }
